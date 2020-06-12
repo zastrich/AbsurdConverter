c372c7d4c8a1642c66f263f8fbf0b71ebf6e0ded
Zastrich <zas@code200.com.br>
2020-06-12 04:18:54 -0300

    Start with changes

---
+++ pages/distances/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Distances() {
+    return (
+        <div className="container">
+            <Head>
+                <title>Distance</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>Distances</h1>
+        </div>
+    )
+}
