246d1dbd6c0dcfd3708aa53d4fcd080cba50e21e
Zastrich <zas@code200.com.br>
2020-06-12 07:11:33 -0300

    Units to compare

--- units/index.js
+++ units/index.js
@@ -1,3 +1,21 @@
-const distances = [
-    {}
+// area: meters²
+// volume: meters³
+// length: meters - Comprimento
+// width: meters - Largura
+// height: meters - Altura
+// weight: kilos - Peso
+// time: hours - Tempo
+
+export default Unit
+
+let Unit = [
+    {name: "Campo de Futebol", area: 11000, length: 110, width: 100, img: ""},
+    {name: "Piscina Olímpica", area: 1250, length: 50, width: 25, height: 2, volume: 2500, img: ""},
+    {name: "Torre Eiffel", height: 324, weight: 17300000, img: ""},
+    {name: "Coração Humano", height: 0.06, width: 0.08, length: 0.12, weight: 0.35, img: ""},
+    {name: "Formiga Lasius niger", length: 0.07, weight: 0.000003, img: ""},
+    {name: "Lhama", height: 1.8, length: 2.4, weight: 200, img: ""},
+    {name: "Alpaca", height: 0.99, length: 1.8, weight: 84, img: ""},
+    {name: "Comprimento do pelo de uma Alpaca", length: 0.2, img: ""},
+    {name: "Vicunha", height: 0.85, length: 1.6, weight: 65, img: ""},
 ]
